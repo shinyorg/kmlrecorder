7af57c010bfd510b30517919e7478364b6055ac6
Allan Ritchie <allan.ritchie@gmail.com>
2026-02-11 22:58:56 -0500

    Fixes

--- ShinyKmlRecorder/Delegates/MyGpsDelegate.cs
+++ ShinyKmlRecorder/Delegates/MyGpsDelegate.cs
@@ -1,17 +1,9 @@
-using System.Data;
 using Shiny.Locations;
-using ShinyKmlRecorder.Services;
 
 namespace ShinyKmlRecorder.Delegates;
 
 
-// this uses a Shiny lifecycle task and runs in the same scope as the GPS delegate
-// if GPS isn't running, this guy will still be watching app state and the timer will keep going (as long as the app is alive)
-public partial class MyGpsDelegate(
-    ILogger<MyGpsDelegate> logger,
-    ILogService logService,
-    IGpsManager gpsManager
-) : IGpsDelegate
+public partial class MyGpsDelegate(ILogService logService) : IGpsDelegate
 {
 
     public Task OnReading(GpsReading reading) => logService.AddLog(reading);
@@ -22,7 +14,7 @@ public partial class MyGpsDelegate : IAndroidForegroundServiceDelegate
 {
     public void Configure(AndroidX.Core.App.NotificationCompat.Builder builder)
     {
-        builder.SetContentText("ClearD Driver Behaviour is tracking you!  Break me if you can...");
+        builder.SetContentText("KML Recorder is doing its thing...recording!");
     }
 }
 #endif
